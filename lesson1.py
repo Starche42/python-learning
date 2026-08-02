c8fdc848399c029a46288c93433cd2bdefdf3bd4
Starche42 <165922423+Starche42@users.noreply.github.com>
2026-08-02 22:17:03 +0300

    Delete lesson1.py

--- lesson1.py
+++
@@ -1,9 +0,0 @@
-name = input("Как тебя зовут?")
-age = input("Сколько тебе лет?")
-language = input("Твой любимый язык программирования?")
-
-print()
-print(f"Привет, {name}!")
-print(f"Тебе {age} лет!")
-print(f"Твой любимый язык - {language}!")
-print(f"У тебя все получится") 
